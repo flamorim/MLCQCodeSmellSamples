9c58121e7a8039fed5f75319f6de36d1b41184d8
sandroacoelho <sandroacoelho@gmail.com>
2022-11-17 09:05:19 -0300

    Fix ck-analisys script

--- scripts/ck-analisys.sh
+++ scripts/ck-analisys.sh
@@ -2,7 +2,8 @@
 for i in $(cat files); do
  echo  "processing $i"
  mkdir -p ../ck_analysis/$i 
- #java -jar ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i  ../ck_analysis/$i
- #git add ck_analysis/$i 
- #git commit -m "add ck analysis for $i" 
+ java -jar ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i true 0 true ../ck_analysis/$i/
+ git add ../ck_analysis/$i 
+ git commit -m "add ck analysis for $i" 
+ git push
 done
