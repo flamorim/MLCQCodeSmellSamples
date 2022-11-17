96e83d5b6636cd3476b388573465dfd0dd6bb33e
sandroacoelho <sandroacoelho@gmail.com>
2022-11-17 10:38:57 -0300

    add ck analysis for azure-chat-for-java_4456bea8830c5edf8c211db1d7816b849372d6ed

--- scripts/ck-analisys.sh
+++ scripts/ck-analisys.sh
@@ -2,7 +2,7 @@
 for i in $(cat files); do
  echo  "processing $i"
  mkdir -p ../ck_analysis/$i 
- java -jar ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i true 0 true ../ck_analysis/$i/
+ java -jar -Xms8g -Xmx8g ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i true 0 true ../ck_analysis/$i/
  git add ../ck_analysis/$i 
  git commit -m "add ck analysis for $i" 
  git push
