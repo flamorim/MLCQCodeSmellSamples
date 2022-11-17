3b38dc32e67e588179dc5d4bb0cdef1e7171a67d
sandroacoelho <sandroacoelho@gmail.com>
2022-11-17 08:55:28 -0300

    add ck analysis for royale-compiler_fbd9bc3b9e48c80dbd8c1d32a6f83221e314efdd

--- scripts/ck-analisys.sh
+++ scripts/ck-analisys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in $(cat files); do
- mkdir -p ck_analysis/$i 
- java -jar ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i  ck_analysis/$i
- git add ck_analysis/$i 
- git commit -m "add ck analysis for $i" 
+ echo  "processing $i"
+ mkdir -p ../ck_analysis/$i 
+ #java -jar ck-0.7.1-jar-with-dependencies.jar  ../dataset/$i  ../ck_analysis/$i
+ #git add ck_analysis/$i 
+ #git commit -m "add ck analysis for $i" 
 done
